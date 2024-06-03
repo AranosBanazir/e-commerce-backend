94e2e0d77bc07b7e32fec43ad4e225701df29863
Caleb Saiia <calebsaiia@gmail.com>
2024-06-02 19:51:29 -0500

    finished product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const products = await Product.findAll()
+  res.status(200).send(products)
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const singleProduct = await Product.findByPk(req.params.id, {
+    include: [{model: Category}, {model: Tag}]
+  })
+  if (!singleProduct){
+    res.status(400).send('No such ID')
+    return
+  }
+  res.status(200).send(singleProduct)
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +58,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -92,8 +102,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+const product = await Product.findByPk(req.params.id)
+const oldProduct = product.dataValues.product_name
+ const del = await Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  if (del === 0){
+    res.status(400).send(`There is no product with the ID '${req.params.id}'`)
+    return
+  }
+
+  res.status(200).send(`${del} product deleted (${oldProduct})`)
+
 });
 
 module.exports = router;
